93370d3c0e49e4458a2cb438937ba93d35f6d818
Scott Schneider <schneider_scott@live.com>
2018-05-03 15:40:55 -0600

    Initial Commit

---
+++ footballController.js
@@ -0,0 +1,30 @@
+function FootballController()
+
+//Private
+var footballService = new FootballService(drawPlayer);
+
+function drawPlayer(chars) {
+    var template = "<h1>searchResults</h1>";
+    for (let i = 0; i < chars.length; i++) {
+        const char = chars[i];
+        template += `
+        <div>
+            <img class="NFLPic" src=
+        `
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Public
