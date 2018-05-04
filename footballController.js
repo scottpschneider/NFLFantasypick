4cb6c442e681f5e63ed5c828734f8f19c6435041
Scott Schneider <schneider_scott@live.com>
2018-05-04 11:34:01 -0600

    firsttimedataispulling

--- footballController.js
+++ footballController.js
@@ -1,31 +1,31 @@
 function FootballController() {
 
     //Private
-    var footballService = new FootballService(drawPlayer);
+    var footballService = new FootballService(drawMyTeam);
 
     function drawMyTeam(players) {
+        console.log(players)
         //this reference to the h1 search results needs to match an id on my index.html yes? can it be a div class or need be h1?
         var template = "<h2>My Team</h2>";
         //by setting that template equal to a tag on html page i am what, setting where that temp will populate?
         for (let i = 0; i < players.length; i++) {
             const player = players[i];
+            //questions about this image class now. class comes from html, also corresponds with a css style. what about thumbnail? 
+            //in marvel thing we did description in a p tag here.  what will this do if i use more h3's, and detail them..?
             template += `
             <div>
-            //questions about this image class now. class comes from html, also corresponds with a css style. what about thumbnail? 
-            <img class="NFLpic" src="${player.thumbnail.path}.${player.thumbnail.extension}" alt="">
-            <h1>Name: ${player.name}</h1>
-            <h3>Position:
-            //in marvel thing we did description in a p tag here.  what will this do if i use more h3's, and detail them..?
-            <h3>Team:  ${player.description ? player.description : "No description!"}</p>
-            <button onclick="app.controllers.footballController.removeFromTeam(${player.id})">Remove From Team</button>
-            the fact that this is all red makes me think something is not right...ha. no really. 
+            <img class="NFLpic" src="${player.photo}" alt="">
+            <h1>Name: ${player.fullname}</h1>
+            <h3>Position: ${player.position}
+            <h3>Team:  ${player.pro_team ? player.pro_team : "unknown"}</p>
+            <button onclick="app.controllers.footballController.removeFromTeam(${player.id})">Remove From Team</button> 
           </div>
 `;
         }
         document.getElementById("myTeam").innerHTML = template
     }
-    function searchResults(players) {
-        //does this need to be a draw function
+    function drawSearchResults(players) {
+        //this is draw function ya
         var template = "<h2>Search Results</h2>";
         for (let i = 0; i < players.length; i++) {
             const player = players[i];
@@ -45,17 +45,27 @@ function FootballController() {
         }
         document.getElementById("searchResults").innerHTML = template;
     }
+//function ItunesController(){
+   // var itunesService = new ItunesService()
+    //Do Not Modify the getMusic function
+    //this.getMusic = function getMusic(e){
+      //e.preventDefault();
+      //var artist = e.target.artist.value;
+      //itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
+   // }
+  
+    //Start coding here
+   // function draw(results){
+    //   console.log(results)
+    // }
+
+    this.searchPlayers = function searchPlayers(e) {
+        e.preventDefault();
+        var query = e.target.name.value
+        var catchResults = footballService.searchName(query)
+        drawSearchResults(catchResults)
+    }
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
--- footballService.js
+++ footballService.js
@@ -1,50 +1,56 @@
-function PlayersService(callback){
-    var playersData = []
-    //...
-    this.getPlayersByTeam = function(teamName){
-      //return an array of all players who match the given teamName.
-    }
-  
-    this.getPlayersByPosition = function(position){
-      //return an array of all players who match the given position.
-    }
-   //...
+function FootballService(callback) {
+  var playersData = []
+  //...
+  this.getPlayersByTeam = function (teamName) {
+    //return an array of all players who match the given teamName.
+  }
 
-   function PlayersService(callback){
+  this.getPlayersByPosition = function (position) {
+    //return an array of all players who match the given position.
+  }
+  //...
+
+  function PlayersService(callback) {
     var playersData = [];
-  
-    this.getPlayersByTeam = function(teamName){
-      return playersData.filter(function(player){
-        if(player.team == teamName){
+
+    this.getPlayersByTeam = function (teamName) {
+      return playersData.filter(function (player) {
+        if (player.team == teamName) {
           return true;
         }
       });
     }
-  } 
-//...
-     function loadPlayersData(){
-       //check if the player already has a copy of the NFL playersData
-       var localData = localStorage.getItem('playersData');
-      //if they do, pull from there
-       if(localData){
-           playersData = JSON.parse(localData);
-           //return will short-circuit the loadPlayersData function
-           //this will prevent the code below from ever executing
-           return callback()
-       }
-       //if not go get that data
-       var url = "https://bcw-getter.herokuapp.com/?url=";
-       var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
-       var apiUrl = url + encodeURIComponent(endpointUri);
- 
-         $.getJSON(apiUrl, function(data){
-           playersData = data.body.players;
-           console.log('Player Data Ready')
-           console.log('Writing Player Data to localStorage')
-           localStorage.setItem('playersData', JSON.stringify(playersData))
-           console.log('Finished Writing Player Data to localStorage')
-           callback()
-         });
-     }    
- loadPlayersData(); //call the function above every time we create a new service
- } 
+  }
+  function searchPlayers(query) {
+    var filteredResults = NFLPlayers.filter(function (player) {
+      return player.name.contains(query)
+    })
+    return filteredResults
+  }
+  //...
+  function loadPlayersData() {
+    //check if the player already has a copy of the NFL playersData
+    var localData = localStorage.getItem('playersData');
+    //if they do, pull from there
+    if (localData) {
+      playersData = JSON.parse(localData);
+      //return will short-circuit the loadPlayersData function
+      //this will prevent the code below from ever executing
+      return callback(playersData)
+    }
+    //if not go get that data
+    var url = "https://bcw-getter.herokuapp.com/?url=";
+    var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+    var apiUrl = url + encodeURIComponent(endpointUri);
+
+    $.getJSON(apiUrl, function (data) {
+      playersData = data.body.players;
+      console.log('Player Data Ready')
+      console.log('Writing Player Data to localStorage')
+      localStorage.setItem('playersData', JSON.stringify(playersData))
+      console.log('Finished Writing Player Data to localStorage')
+      callback(playersData)
+    });
+  }
+  loadPlayersData(); //call the function above every time we create a new service
+} 
